9707836ab85eca8b35d5206462cb6a67e05d68fe
yairschneid <yairschneid@gmail.com>
2017-05-12 14:50:49 +0300

    Initial commit

---
+++ app/src/main/java/thegang/aistq2017/MainActivity.java
@@ -0,0 +1,15 @@
+package thegang.aistq2017;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import static thegang.aistq2017.R.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_main);
+    }
+}
